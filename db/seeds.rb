6e1fd8323fa814fbce3d2719da89622dcea2a726
JakariaJishan <jakariajishan987@gmail.com>
2023-09-27 14:44:03 +0600

    Create associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.create(name:"jack")
+user2= User.create(name:"joe")
+
+post1 = Post.create(title:"post1", content:"this is post1", user_id: user.id)
+post2 = Post.create(title:"post2", content:"this is post2", user_id: user.id)
+
+Comment.create(body:"this comment is for post 1", user_id:user.id, post:post1)
+Comment.create(body:"this comment is for post 2", user_id:user2.id, post:post2)
