2afe547d6a91ab3e31f738ea69fab4ab28e96d92
JakariaJishan <jakariajishan987@gmail.com>
2023-09-27 20:39:32 +0600

    Implement update and destroy comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -22,6 +22,30 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to post_path(@post)
+    end
+
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      redirect_to post_path(@post)
+    else
+      redirect_to post_path(@post), status: :unprocessable_entity
+    end  end
+
   private
 
   def comment_params
