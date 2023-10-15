65324713fe2fed85e2ce890c7b75edcc5956c000
JakariaJishan <jakariajishan987@gmail.com>
2023-10-15 15:17:03 +0600

    Add popup to delete and edit

--- app/javascript/popup.js
+++ app/javascript/popup.js
@@ -1,6 +1,12 @@
 const open_btn = document.getElementById('open')
 const popup = document.getElementById('popup')
 
+const post_dot =  document.getElementById('post-dot')
+const post_popup = document.getElementById('post-popup')
+
+const comment_dot = document.getElementById('comment-dot')
+const comment_popup = document.getElementById('comment-popup')
+
 open_btn?.addEventListener('click', (e)=>{
     popup.classList.toggle('hidden')
 
@@ -8,9 +14,23 @@ open_btn?.addEventListener('click', (e)=>{
     // location.reload()
 })
 
+post_dot?.addEventListener('click', (e) =>{
+    post_popup.classList.toggle('hidden')
+    e.stopPropagation()
+})
+comment_dot?.addEventListener('click', (e) =>{
+    comment_popup.classList.toggle('hidden')
+    e.stopPropagation()
+})
+
 window.addEventListener('click', e =>{
     if(!popup?.contains(e.target)){
         popup?.classList.add('hidden')
     }
+    if(!post_popup?.contains(e.target)){
+        post_popup.classList.add('hidden')
+    }
+    comment_popup.classList.add('hidden')
 })
+
 
