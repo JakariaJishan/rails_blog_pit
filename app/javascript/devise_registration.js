0337d1050d9a6ef08bd4288fdadea96dcec23c47
JakariaJishan <jakariajishan987@gmail.com>
2023-10-20 16:41:13 +0600

    Add image preview in create post

--- app/javascript/devise_registration.js
+++ app/javascript/devise_registration.js
@@ -1,6 +1,21 @@
+
+let new_post_input = document.getElementById('new-post')
+let post_preview = document.getElementById('preview-post')
+new_post_input.addEventListener('change', (e) =>{
+   let file = e.target.files[0];
+   let reader = new FileReader();
+   reader.onload = function () {
+      post_preview.src = reader.result;
+   }
+   reader.readAsDataURL(file)
+})
 let phone_input = document.getElementById('phone-input')
-intlTelInput(phone_input, {
-   utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.2.1/build/js/utils.js",
-   separateDialCode: true,
-   initialCountry: "bd",
-});
+console.log(phone_input)
+if(phone_input){
+   intlTelInput(phone_input, {
+      utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.2.1/build/js/utils.js",
+      separateDialCode: true,
+      initialCountry: "bd",
+   });
+}
+
