05c420a88bc5a5f9d6bce27f40776f6929be9237
JakariaJishan <jakariajishan987@gmail.com>
2023-10-31 12:34:29 +0600

    remove rebroadcasting #2

--- app/javascript/channels/chat_channel.js
+++ app/javascript/channels/chat_channel.js
@@ -1,6 +1,6 @@
 import consumer from "./consumer"
 
-const chatChannel = consumer.subscriptions.create("ChatChannel", {
+consumer.subscriptions.create("ChatChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
     console.log('connected')
