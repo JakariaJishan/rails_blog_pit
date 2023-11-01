ae72248c1089f3a5e15147b6bc5194fe81db6c8a
JakariaJishan <jakariajishan987@gmail.com>
2023-11-01 17:21:36 +0600

    Design user sidebar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   end
 
   def show
+      @users = User.all
       @user = User.find(params[:id])
       @messages = Message.where(sender_id: current_user.id, recipient_id: @user.id)
                          .or(Message.where(sender_id: @user.id, recipient_id: current_user.id))
