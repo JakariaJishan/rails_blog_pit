a8df8acc1d820fb542a1c2aeab5f4116ed23b82c
JakariaJishan <jakariajishan987@gmail.com>
2023-11-03 15:52:02 +0600

    Customize design to chat show page

--- app/javascript/channels/user_presence_channel.js
+++ app/javascript/channels/user_presence_channel.js
@@ -14,11 +14,17 @@ document.addEventListener('DOMContentLoaded', function() {
     received(data) {
       console.log(data)
       const active_user = document.getElementById(data.user)
+      const green_dot = document.getElementById(`green_dot_${data.user}`)
       if (data.onLine === 'on'){
         active_user?.classList.remove('hidden')
+        green_dot?.classList.remove('hidden')
         active_user?.classList.add('bg-green-500')
+        green_dot?.classList.add('bg-green-500')
+
       }else{
         active_user?.classList.add('hidden')
+        green_dot?.classList.add('hidden')
+
       }
       // Called when there's incoming data on the websocket for this channel
     }
