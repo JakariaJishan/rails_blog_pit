6f58f552a15c49e9e65ea4ac415f86946e0e9f7a
JakariaJishan <jakariajishan987@gmail.com>
2025-01-19 23:25:40 +0600

    changes to be committed

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,19 +1,19 @@
-# class ChatsController < ApplicationController
-#   before_action :authenticate_user!
-#   def index
-#     @chats = Chat.all
-#   end
-#
-#   def create
-#     @chat = current_user.chats.new(chat_params)
-#     @chat.save
-#     ActionCable.server.broadcast("chat_channel", @chat.as_json(include: :user))
-#
-#   end
-#
-#   private
-#
-#   def chat_params
-#     params.require(:chat).permit(:body)
-#   end
-# end
+class ChatsController < ApplicationController
+  # before_action :authenticate_user!
+  # def index
+  #   @chats = Chat.all
+  # end
+
+  # def create
+  #   @chat = current_user.chats.new(chat_params)
+  #   @chat.save
+  #   ActionCable.server.broadcast("chat_channel", @chat.as_json(include: :user))
+
+  # end
+
+  # private
+
+  # def chat_params
+  #   params.require(:chat).permit(:body)
+  # end
+end
